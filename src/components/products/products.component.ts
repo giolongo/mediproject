ae0dd39904447fd24a1db86eec678338f35fad93
giolongo <giovanniemanuelelongo@gmail.com>
2025-07-26 12:57:07 +0200

    feat: hover effet on card image

--- src/components/products/products.component.ts
+++ src/components/products/products.component.ts
@@ -3,13 +3,22 @@ import { ResourcesService } from '../../service/resources.service';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-products',
   standalone: true,
   imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
+  animations: [
+    trigger('slideInFromLeft', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(-30px)' }),
+        animate('700ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+      ])
+    ])
+  ]
 })
 export class ProductsComponent {
 
