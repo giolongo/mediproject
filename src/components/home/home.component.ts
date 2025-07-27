e785c0dcc0c7f1dcc7ab9d0562d5916ce35702be
giolongo <giovanniemanuelelongo@gmail.com>
2025-07-27 10:23:20 +0200

    fix: home page image size

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { trigger, style, animate, transition } from '@angular/animations';
 import { ResourcesService } from '../../service/resources.service';
@@ -22,27 +22,55 @@ import { ResourcesService } from '../../service/resources.service';
   ]
 })
 export class HomeComponent implements OnInit {
-
-  private resourcesService = inject(ResourcesService)
+  private resourcesService = inject(ResourcesService);
   resources = this.resourcesService.resources;
 
   public currentImage: any;
   public index = 0;
 
-
   ngOnInit(): void {
-    this.currentImage = this.resources()[this.index];
+    this.setImage(this.resources()[this.index]);
     this.index++;
 
     setInterval(() => {
       this.currentImage = null;
       setTimeout(() => {
-        this.currentImage = this.resources()[this.index];
+        this.setImage(this.resources()[this.index]);
         this.index++;
         if (this.index >= this.resources().length) {
           this.index = 0;
         }
       }, 500);
     }, 5000);
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    // Riapplica l’immagine con orientamento corretto se serve
+    if (this.currentImage?.original) {
+      this.setImage(this.currentImage.original);
+    }
+  }
+
+  private isMobilePortrait(): boolean {
+    return window.innerWidth <= 768 && window.innerHeight > window.innerWidth;
+  }
+
+  private getImageVariant(src: string): string {
+    const ext = src.split('.').pop();
+    const base = src.replace(/\.\w+$/, '');
+    if (this.isMobilePortrait()) {
+      return `${base}_v.${ext}`;
+    }
+    return `${base}.${ext}`;
+  }
+
+  private setImage(image: any): void {
+    const newSrc = this.getImageVariant(image.src);
+    this.currentImage = {
+      ...image,
+      src: newSrc,
+      original: image // salva l'originale per eventuale resize
+    };
   }
 }
