729016f13270cf5211cb9eb5208c4d8fd3ccc2ea
giolongo <giovanniemanuelelongo@gmail.com>
2025-07-30 19:39:36 +0200

    chore: tuning products

--- src/app/models/resource-detail.model.ts
+++ src/app/models/resource-detail.model.ts
@@ -2,6 +2,6 @@ export interface ResourceDetailModel {
     id: number;
     title: string;
     description: string;
-    techical_data: [{label: string; value: string}];
+    technical_data: [{label: string; value: string}];
     src: string;
 }
